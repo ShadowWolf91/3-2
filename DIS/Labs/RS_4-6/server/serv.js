a71ff053227980d3746d8cfd1794ca2339d2b6a5
ShadowWolf91 <90034634+ShadowWolf91@users.noreply.github.com>
2023-05-24 14:26:06 +0300

    AЗiPL Reports

--- DIS/Labs/RS_4-6/server/serv.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize');
-const config = require('./config.json');
-const db = require('./context')(Sequelize, config);
-const server = require('./server')(db, config);
-
-server.listen(8005, () =>console.log('Server is running'));
